896cef8a5f2d2058d7de3973825f2c942cdf9f3a
min <myun02302@gmail.com>
2023-01-23 16:08:20 +0900

    #3-db연결 문제 해결

--- src/main/java/com/project/sns/controller/response/UserJoinResponse.java
+++ src/main/java/com/project/sns/controller/response/UserJoinResponse.java
@@ -3,7 +3,9 @@ package com.project.sns.controller.response;
 import com.project.sns.model.User;
 import com.project.sns.model.UserRole;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public class UserJoinResponse {
 
