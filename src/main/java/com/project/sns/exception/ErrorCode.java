802a5faa5c84f1c448b5851d4e091a072f7ba32b
min <myun02302@gmail.com>
2023-01-31 00:14:57 +0900

    #17-토큰정보없을시 포스트작성하면 반환하는 에러 코드 수정

--- src/main/java/com/project/sns/exception/ErrorCode.java
+++ src/main/java/com/project/sns/exception/ErrorCode.java
@@ -11,7 +11,9 @@ public enum ErrorCode {
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "Username is duplicated"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded"),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error")
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
+
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED,"Token is invalid"),
 
     ;
     private HttpStatus status;
